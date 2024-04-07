3982ef94e3562c45b4307819990cff320917969b
OkamiWolf027 <162058528+OkamiWolf027@users.noreply.github.com>
2024-04-07 11:36:58 -0500

    Update mental_health.py

--- mental_health.py
+++ mental_health.py
@@ -1,1 +1,122 @@
+disorder={
+	"Depression":["Consistent Treatment","Stay In Touch","Be More Active","Face Your Fears (Depression)","Limit Intoxication","Healthy Diet","Sleep Routine","Seek Help"],
+	"Anxiety":["Consistent Treatment","Stress Management and Relaxation Techniques.","Learning About It","Identifying Triggers (Anxiety)","Keeping A Journal (Anxiety)"],
+	"Paranoia":["Keep A Journal (Paranoia)","Face Your Fears (Paranoia)","Identifying Triggers (Paranoia)","Communication"],
+	"Panic Attacks":["Relax your Breathing and Muscles","Exercise (Panic Attacks)","Identifying Triggers (Panic Attacks)","Face Your Fears (Panic Attacks)"],
+	"Eating Disorders":["Mindfulness","Make a Meal Plan","Create Routines","Seek help"],
+	"Schizophrenia":["Medication","Community","Eliminate Unhealthy Habits"],
+	"Bipolar Disorder":["Keep a Routine","Do What You Love","Exercise (Bipolar)"],
+	"PTSD":["Exercise (PTSD)","Mindfulness","Talk to Others"],
+	"OCD":["Recenter Yourself","Relax","Move Your Body"],
+	"Dissociative Identity":["Awareness and Acceptance","Triggers","Stay In The Present","Daily Routine","Relaxing"]
+} # the the disorder and the coping mechanisms that work with it
 
+symptoms={
+	"Depression":"Depression is a very common mental illness and a very deadly one at that. It can range wildly from feeling sad, hopeless, tearful, angry, self-blame, guilty, or numb. You can easily outburst in anger, and become easily irritable or frustrated over small details. You can lose interest in things you love, have little to no motivation, and lack energy to do anything, even small little things. Under/Overeating is very common, as well as suffering from insomnia and/or sleeping too much. Feeling sluggish/not wanting to move. Feelings of worthlessness or guilt, fixating on past failures or self-blame. Trouble thinking, concentrating, making decisions, and/or remembering things. Frequent and/or recurrent thoughts of death, suicidal thoughts, suicide attempts, or suicide. Experiencing unexplained physical problems, such as back pain or headaches. All of these are normal and not to be ashamed of!",
+	"Anxiety":"Anxiety often includes constant worrying, restlessness, being easily fatigued, and having difficulty concentrating. Irritability and disturbances in sleep are also common.",
+	"Paranoia":"Paranoia involves being overly suspicious and feeling like everything is against you or your friends. You may have difficulty trusting others or be very sensitive.",
+	"Panic Attacks":"A panic attack is characterized by a sudden intense fear, as well as an increase in heart rate and rapid breathing.",
+	"Eating Disorders":"Common symptoms of eating disorders include focusing on food, calories, and weight loss, refusal to eat certain foods, or just avoiding mealtimes altogether. Someone with an eating disorder may also isolate themselves from others, become extremely concerned with their body’s appearance, and have severe mood swings. ",
+	"Schizophrenia":"Having Schizophrenia can be very scary. You can experience a wide range of hallucinations, delusions, sensory distortions, and/or loss of a sense of reality. This can disturb your sleep, cause you to hear whispers or voices in your head, and experience paranoia. This can lead to making you believe that you’re going insane and may cause you to act irrationally.",
+	"Bipolar Disorder":"Bipolar disorder is made up of extreme mood swings, typically having a high point where one feels happy and energetic or unusually irritable and a low point when depression sets in. These mood swings can occur rarely or they could be multiple times in a year.",
+	"PTSD":"PTSD, or Post-Traumatic Stress Disorder, happens after a traumatic experience of some sort. Common symptoms include a constant fear for your safety and a feeling of sadness and anxiety. You may also relive the triggering event and experience some detachment form others, avoiding anything that might remind you of your trauma.",
+	"OCD":"OCD, or Obsessive-Compulsive Disorder, is a condition that includes frequent unwanted thoughts or obsessions, as well as repetitive actions known as compulsions.",
+	"Dissociative Identity":"Dissociation can be recognized through a few signs. It can be seen as having a “split personality” within yourself and switching between them. It can include such things as having independent qualities/traits, behaviors, thoughts, and perceptions of the world. You may even go by a different name. This can be incredibly terrifying, and can especially be extremely distressing if you lose control or awareness when one of your distorted personalities appears. You might wake up in a new location and learn that you went there in an emotional state. Or perhaps you frequently experience a sense of detachment from yourself, watching on helplessly as some part of you acts out on their own. Even if you don’t completely lose control, you might hear multiple voices in your head trying to influence your decision-making. That is okay and you’re not going insane. Your mind is simply fighting against itself, and you need to calm it down and form an alliance between them. Control you. You are the one in control, not them. You may feel broken, shame, or even self-loathing, causing you to feel alone or isolated."
+} # The symptoms for each mental dissorder
+
+cope={
+	# Depression and Paranoia
+	"Consistent Treatment":"Take medications as directed. Little things can go a long way! Keep therapy appointments and complete any assignments your therapist gives you. Consistency can make a big difference, especially when it comes to taking medication.",
+
+	# Depression
+	"Stay In Touch":"Don't withdraw from life. Socializing is an AMAZING way to improve your mood. Keeping in touch with friends and/or family is crucial and means you have someone to talk to when you feel horrible. Humans are very lonely creatures. If you don’t talk to a lot of people, you’re guaranteed to end up feeling lonely, and can and will destroy your health. Try reaching out to a friend and ask to hang out, or even play a few games together. And if you don’t have any friends, make new ones! There are 7.95 BILLION people out there. I guarantee at least one of them will want to be your friend.",
+	"Be More Active":"Exercise is important, for both your physical, AND mental health. By doing so, it can help lift your mood for the better. If you haven't exercised recently or don’t have the motivation to, start small by walking for 15-20 minutes every day! Vitamin D is great for your body and your soul.",
+	"Limit Intoxication":"For some people, alcohol can become a huge problem. You may drink more than usual as a way of coping with or hiding your emotions, or just to fill time. But alcohol won't help you solve your problems and can make you feel more depressed. It destroys your body and is in no way a good way to deal with your pain.",
+	"Healthy Diet":"Some don't feel like eating when they're depressed and are at risk of becoming underweight. Others find comfort in food and can put on excess weight. Both under and over-eating are very risky for your body. Give it some love! Take care of it, and it’ll take care of your mood. Eating healthy makes you feel more motivated and energized. (Though eating a bit of sweets/comfort food every day won’t hurt!)(Antidepressants can also affect your appetite, so please seek help from a doctor if you see signs of this.)",
+	"Sleep Routine":"When people feel down, they can get into poor sleep patterns, staying up late and sleeping during the day. Try your best to wake up at around 6 AM-10 AM, and go to bed at 9 PM-11 PM. Stick to your routine as much as possible!Not having a routine can affect your eating. Try to carry on cooking and eating regular meals.",
+	"Seek Help":"if you're still feeling down or depressed after a couple of weeks.Treatments for depression include talking therapies and antidepressants.You can refer yourself for talking therapies like cognitive behavioral therapy or counseling.",
+
+	# Anxiety
+	"Stress Management and Relaxation Techniques.":"Use stress management and relaxation techniques.Visualization techniques, meditation, and yoga are examples of relaxation techniques that can ease anxiety. Breathe in deeply, and calm down. Everything is going to be okay. Listen to some calming music to relax and calm your nerves.",
+	"Learning About It":"Talk to your healthcare provider/Doctor to find out what might be causing your specific condition and what treatments might be best for you. Involve your family and/or friends, and ask for their support!",
+
+	# Double Paranoia
+	"Face Your Fears (Depression)":"Don't avoid the things you find difficult. When you feel bad or anxious, you usually tend to avoid talking to other people. Some people can lose their confidence in going out, driving, or traveling. If this starts to happen, facing up to these situations will become much easier!",
+	"Face Your Fears (Paranoia)":"You may find that you avoid certain places, people, or situations because of your paranoia. To help with this, look for situations to test out these fears. This might feel scary at first. But you can start small, and build up gradually!For example, let’s say you have paranoid thoughts about your home getting robbed. Locking your doors every night is a good habit to keep, especially if you live in a dangerous area. Keeping a light on in your kitchen can keep your paranoia at bay, and turning on white noise can calm your nerves instead of listening to dead quiet. Keeping a dog is also a great way to calm your nerves. Your little fluffy friend can alert you if they hear any unusual noises!",
+	"Face Your Fears (Panic Attacks)":"Facing your fears may be difficult, but if you can stand up to the source of your panic attacks, you can train yourself to not be afraid of it anymore. You don’t have to do it alone either, so feel free to bring a friend!",
+	"Identifying Triggers (Anxiety)":"Learn what causes you stress or anxiety! Use those Stress and relaxation techniques you learned so you're ready to deal with anxious feelings in those situations.",
+	"Identifying Triggers (Paranoia)":"What actions or words trigger you? Writing down and identifying it can help you tackle your paranoia. Recognize when you’re having them, and what they’re about, and use coping mechanisms to calm yourself down.",
+	"Identifying Triggers (Panic Attacks)":"Knowing what causes you to panic can help prevent panic in the first place. Write down your thoughts whenever you feel panicked or talk to a friend and get to the bottom of it.",
+	"Keeping A Journal (Anxiety)":"Keep track of your personal life so it can help you remember what's causing you stress and what seems to help make you feel better!",
+	"Keeping A Journal (Paranoia)":"Write down what your paranoid thoughts are about, when you have them, how they make you feel/how distressing they are, how often you think about them, and your sleep schedule. If your mind feels scrambled, write them down! Then you’ll have one less thing on your mind.",
+
+	# Paranoia
+	"Communication":"Finding someone to talk about your thoughts with someone can reduce stress and help you to question and challenge paranoid thoughts. If you're not ready to talk yet, just spending time with other people may help you to feel less isolated. You can share this information with your friends and/or family about your paranoia, so they can comfort and support you!This may be difficult as paranoid thoughts can often lead to distrust of other people. If you don't have someone you feel you can trust, try calling a Hotline! It may seem scary, but they’re there to help you. And if you dial the wrong one, don’t worry! They’ll help you to talk to the right person.",
+
+	# Panic Attacks
+	"Relax your Breathing and Muscles":"Relaxing your breathing and body can be very helpful when panic sets in, and it can also help stop them from happening in the first place!",
+
+	#Eating Disorder
+	"Mindfulness":"Focus your thoughts on the present and don’t worry about the past or future. It can help take your mind off of things that would cause disorders in the first place.",
+	"Make a Meal Plan":"Work on a proper meal plan for breakfast, lunch, dinner, and a few snacks. Work with a friend or family member to make sure that it is healthy and works for you.",
+	"Create Routines":"Keeping a proper routine can be very helpful for disorders. Doing so can help take your mind off of any stress or pressure you may feel and lead to a healthier lifestyle.",
+	"Seek help":"You may need someone to help control your eating habits. Talk to a therapist or trusted friend or family member to work it out.",
+
+	#Schizophrenia
+	"Medication":"Medication for Schizophrenia is crucial for your mental health. Take antipsychotics if your doctor recommends them. Taking medications can be scary, but it will help you so much in the future.",
+	"Community":"Connecting with people who have similar symptoms can prove very helpful and efficient. Learning from others about how to deal with Schizophrenia is a great way to calm your mind, and find effective ways to deal with it. Having friends and/or family know about your conditions can prove very beneficial!",
+	"Eliminate Unhealthy Habits":"Continuing smoking, excessive drinking, or taking drugs can and will destroy your body. It destroys you and is not an effective method. It won’t take the pain away and it will only make it worse. Those with schizophrenia are three times more likely to smoke, and alcohol and drugs can contribute to worsening psychosis symptoms. When you stop taking them, sure, you won’t feel better, but at least you’re not willingly making it worse.",
+
+	#Double Bipolar
+	"Exercise (Panic Attacks)":"Panic attacks can make you feel tired, but a little exercise can make you feel so much better! Take a quick walk or maybe do some stretching or yoga.",
+	"Exercise (Bipolar Disorder)":"Moving your body can be a great way to manage bipolar disorder, and it can help take your mind off of things too.",
+	"Exercise (PTSD)":"Moving your body can help take your mind off of stress and past experiences. It can also help energize you.",
+	
+	#Bipolar
+	"Keep a Routine":"A routine can help manage bipolar disorder. The less you have to stress about what comes next, the more you can focus on other things that you enjoy.",
+	"Do What You Love":"Doing something that makes you happy can go a long way, and it helps alleviate stress and other triggers.",
+
+	#PTSD
+	"Mindfulness":"Being mindful means focusing on the present, rather than the past or future. This can help eliminate the stress that comes with dwelling on traumatic memories.",
+	"Talk to Others":"Sometimes it takes talking to other people to relieve the pressure of trauma. Getting it off of your chest can be very helpful, especially when you are suffering under the weight of these memories.",
+	#OCD
+	"Recenter Yourself":"If you catch yourself obsessing over something, stop and take a moment to refocus. Once you do, it will be easier to manage.",
+	"Relax":"Take a moment to de-stress. Do some breathing exercises, yoga, or anything else that calms you. Once you are free from stress, you can properly focus on what needs to be done.",
+	"Move Your Body":"Exercising can be a great way to take your mind off of obsessions, and the movement can be beneficial to your health while dealing with mental health.",
+
+	#Dissociative Identity
+	"Awareness and Acceptance":"Accept your different personalities without judgment. This won’t happen overnight, and you may need help from a therapist to achieve it, but self-compassion and acceptance aren’t impossible and are within your grasp. No one can fall too far and not recover. You may feel uncomfortable or need to avoid them at all costs. Learning to accept, or at least acknowledge, all parts of yourself can prove beneficial and even lay the groundwork for future therapy. For example, you might have a trauma-stuck alter that feels trapped in a moment of past danger or hurt that needs constant soothing. Accept them. It’ll be okay, for both you, and them.",
+	"Triggers":" Acknowledge your distortions and recognize how/when they appear. What triggers them? Recognizing when a “switch” occurs can help you identify and prepare for things that trigger you. It can be related to specific people, words, times, places, senses, etc. You may be able to avoid some triggers altogether, but it’s only temporary. It can lead to unhealthy habits, such as becoming intoxicated and taking drugs to cope. There are many healthier alternatives that you can do to help.",
+	"Stay In The Present":"Try to exaggerate your senses. Observe your surroundings, focus on objects and sounds, touch textures, and associate them with similar objects. Exercise the use of taste and smell. It may seem childish, but it will help you acknowledge the real world and can help you avoid spacing out. ",
+	"Daily Routine":"Have a predictable routine.  Wake up, shower, and eat breakfast around the same time each day. When possible, be consistent with exercise, screen time, and your work schedule! Make a habit of using a calendar or planner,  and if you experience a memory lapse, one glance at your planner can help you determine what you’ve completed and what comes next in your day! Don’t be too strict with your schedule. Some people use a full schedule as a way to avoid feeling their emotions. However, this can lead to mental, physical, and emotional exhaustion. Set aside time to relax!",
+	"Relaxing":"Try slowly relaxing the muscles in your body. Strain parts of your body and gradually relax each body part, focusing on one area at a time.Visualization exercises rely on the power of your imagination to set a soothing scene. When it comes to DID, you might find it helpful to include your alters as part of the exercise. Imagine a peaceful, beautiful area you’d like to go or see! Use breathing exercises to soothe your body and calm down. Breathe in and out slowly, or whatever is comfortable for you! It might help to imagine all parts of you breathing together in unison. Feel the air filling your lungs, and then slowly breathe out. Repeat until your nerves have calmed down!",
+} # stores more information about each of the diffrent coping mechanisms
+
+def get_disorder_list(): # gets all the disroders and puts them in a list
+	global disorder
+	list=[]
+	for x in disorder:
+		list.append(x)
+	return list
+
+def remove_parentheses(list): # removes the perentheses and everything in it from every item in a list
+	new_list=[]
+	for x in list:
+		if("(" in x):
+			x=x[:x.index("(")-1]
+		new_list.append(x)
+	return new_list
+
+def add_parentheses(thing, add):
+	if(thing in ("Face Your Fears","Identifying Triggers","Keeping A Journal","Exercise")):
+		thing = thing +" ("+add+")"
+	return thing
+
+def get_cope_list(show_which_disorder): # gets all the coping mechanisms and puts them in a list
+	global cope
+	list=[]
+	for x in cope:
+		if("(" in x and show_which_disorder==False):
+			x=x[:0-x.index("(")-1]
+		list.append(x)
+	return list
